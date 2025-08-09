6808b1c6eda31788c7440bf087faefa080f0cd4a
Magucero <s.pereyragallardo@gmail.org.ar>
2025-08-09 15:23:47 -0300

    posteos model to db and more

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-from models import User
+from models import User,Posts
 
 @login_manager.user_loader 
 def load_user(user_id):
@@ -37,6 +37,56 @@ def load_user(user_id):
 def index():
     return render_template(
         'index.html'
+    )
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for(
+        'index')
+    )
+
+@app.route('/posts',methods =['POST','GET'])
+def posts():
+    if request.method == 'POST':
+        now = datetime.now()
+        tittle = request.form['tittle']
+        content = request.form['content']
+        
+        
+        post = Posts(
+        tittle = tittle,
+        content = content,
+        date = now,
+        user_id = current_user.id
+        )
+
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('index'))
+    
+    return render_template(
+        'posts.html'
+    )
+
+
+
+@app.route('/login',methods= ['GET' , 'POST'])
+def login():
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('login'))
+    
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password'] # pass que llega desde el formulario
+        
+        user = User.query.filter_by(username = username).first()
+        if user and check_password_hash(pwhash = user.password_hash, password=password):
+            
+            login_user(user)  
+            return redirect(url_for('posts'))
+        
+    return render_template(
+        'auth/login.html'
     )
 
 @app.route('/register',methods=['GET', 'POST'])
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from app import db
+#agregar hora incoming
 from flask_login import UserMixin
 
 
@@ -8,6 +9,23 @@ class User(db.Model,UserMixin):
     email = db.Column(db.String(100),nullable=False,unique=True)
     password_hash = db.Column(db.String(256),nullable=False)
     is_active = db.Column(db.Boolean, default = True)
+    posts = db.relationship(
+        'Posts',
+        backref = 'user',
+        lazy = True
+    )
 
     def __str__(self):
         return self.username
+    
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    tittle = db.Column(db.String(100),nullable=False)
+    content = db.Column(db.String(256),nullable=False)
+    date = db.Column(db.DateTime, nullable= False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def __str__(self):
+        return f'{self.tittle}-{self.date}-{self.content}- '
